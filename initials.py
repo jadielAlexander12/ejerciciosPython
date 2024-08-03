36fd1c8cefb8b4610b5c7e165102f280e8103a2f
jadiel <jadiel.gomez@unisa.edu.mx>
2024-08-03 15:15:26 -0600

    Ejercicios sesión 5

---
+++ initials.py
@@ -0,0 +1,12 @@
+print("    DDDDDDDDD      LLLL")
+print("       DD         LL  LL")
+print("       DD        LL    LL")
+print("       DD      LL       LL")
+print("       DD      LL         LL")
+print("       DD     LL LLLLLLLLL LL")
+print("       DD    LL             LL")
+print(" DDDDDDD    LL               LL")
+print
+
+
+
---
+++ temperatura.py
@@ -0,0 +1,5 @@
+temp =int(input("¿cual esla temperatura actual?"))
+
+result= (temp - 32)/ 1.8
+
+print("la temperatura en grados fahrenheit es de:", result) 
